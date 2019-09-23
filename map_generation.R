3e44f15741efd97061e174a5466436278f72062f
unknown <selmeczi.toth.adam@gmail.com>
2019-09-23 23:36:31 +0200

    added export folder

--- map_generation.R
+++ map_generation.R
@@ -21,32 +21,19 @@ setwd("C:/Users/Adam/Documents/budapest_map/hungary_map")
 city <- "budapest"
 lat <- 47.506708 #center point latitude
 long <- 19.046259 #center point longitude
-rad <- 2000 #radius, in meters, around the center point to map
+rad <- 7000 #radius, in meters, around the center point to map
 crs <- 102013 #ESRI projection for mapping. I found mine here: https://spatialreference.org/ref/esri/europe-albers-equal-area-conic/ 102013 will give good results for Europe.
 
 
 #-----------set up the road types you want to plot and what colors they should be
 plottypes <-  c('Utca', 'Tér', 'Út', 'Körút', 'Rakpart', 'Lépcső‘', 'Híd', 'Sétány', 'Vizek')
-plotcolors <-  c('Utca' = '#5EC3E1', 'Tér' = '#FFD035', 'Út' ='#0D7ABF', 'Körút' = '#2E968C', 'Rakpart' = '#EA4E66',
-                'Híd' = '#4EB480', 'Sétány' = '#F3902C', 'Egyéb' = '#C9C9C9', 'Vizek' = '#F39CA6')
+plotcolors <-  c('Utca' = '#5EC3E1', 'Tér' = '#FFD035', 'Út' ='#4EB480', 'Körút' = '#2E968C', 'Rakpart' = '#EA4E66',
+                'Híd' = '#0D7ABF', 'Sétány' = '#F3902C', 'Egyéb' = '#C9C9C9', 'Vizek' = '#F39CA6')
 
 #-----------get to plotting
 #import  road geography
 filename <- "gis_osm_roads_free_1"
 allroads_raw <- read_sf(".", filename)
-
-#waters
-waters <- read_sf(".", "gis_osm_water_a_free_1")
-waters_sub <- st_intersection(circle, waters) 
-
-waters_sub$TYPE <- "Vizek"
-waters_sub$ref <- NA
-waters_sub$oneway  <- NA
-waters_sub$maxspeed <- NA
-waters_sub$layer <- NA
-waters_sub$bridge <- NA
-waters_sub$tunnel <- NA
-waters_sub$len <- NA
 
 
 #subset the roads into a circle.
@@ -55,6 +42,10 @@ pt <- pt %>% st_as_sf(coords = c("long", "lat"), crs = 4326) %>%  st_transform(c
 circle <- st_buffer(pt, dist = rad)
 circle <- circle %>% st_transform(st_crs(allroads_raw))
 allroads_circle <- st_intersection(circle, allroads_raw)
+
+#waters
+waters <- read_sf(".", "gis_osm_water_a_free_1")
+waters_sub <- st_intersection(circle, waters) 
 
 #remove unnamed footpaths
 allroads_circle <- allroads_circle[!(allroads_circle$fclass  == "footway" & is.na(allroads_circle$name)),]
@@ -124,15 +115,20 @@ ggplot() + blankbg + theme(panel.grid.major = element_line(colour = "transparent
   scale_color_manual(values = plotcolors, guide = "legend") 
 
 
-ggplot() + blankbg + theme(panel.grid.major = element_line(colour = "transparent")) +
+map_des <- ggplot() + 
+  blankbg + theme(panel.grid.major = element_line(colour = "transparent")) +
   geom_sf(data=waters_sub, fill = '#F39CA6', color = '#F39CA6', alpha = .2)+
-  #geom_sf(data=otherroads, size = .8, aes(color=TYPE)) + 
   geom_sf(data=allroads, size =1, aes(color=TYPE)) +
   scale_color_manual(values = plotcolors, guide = "legend") 
 
+save_path <- ("C:/Users/Adam/Documents/budapest_map/exports")
 
-
-ggsave(paste0(".", city, ".png"), plot = last_plot(),
-       scale = 1, width = 24, height = 36, units = "in",
+ggsave(paste0(city,"3", ".png"),
+       plot = map_des,
+       path = save_path,
+       scale = 1,
+       width = 24,
+       height = 36,
+       units = "in",
        dpi = 500)
 
